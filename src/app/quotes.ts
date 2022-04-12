2c7989786780eb7c34318917ab6f3ff869f7a9ca
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-04-12 23:12:29 +0300

    modify changes to the form

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,19 @@
 export class Quotes {
+    id: number;
+    name: string;
+    publisher: string;
+    quote: string;
+    date: number;
+    upvotes: number;
+    downvotes: number;
+
+    constructor (id:number, name: string, publisher: string, quote: string, date: number, upvotes: number, downvotes: number) {
+        this.id = id;
+        this.name = name;
+        this.publisher = publisher;
+        this.quote = quote;
+        this.date = date;
+        this.upvotes = upvotes;
+        this.downvotes = downvotes;
+    }
 }
