b389723e5439400d88cba092e157ce00dc53395b
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-04-13 10:50:41 +0300

    add the toogle button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -12,7 +12,12 @@ export class QuotesComponent implements OnInit {
     new Quote (2, "Belinda", "Malomba", "Quiting is never an option, focus on the prize",new Date (2022/3/15),0,0 ),
     new Quote (3, "Nelson Mandela", "Moses Ochieng", "It always seems impossible until its done", new Date (2022/4/6), 0, 0),
     new Quote (4, "Oscar Wilde", "Shana", "Be yourself; everyone else is already taken", new Date (2022/3/20), 0, 0),
+    new Quote (5, "Dr.Jeremy Goldwig", "Reagan", "Live a life that would make you jelous", new Date (2022/4/12), 0,0),
   ]
+
+  toggleDetails(index: string | number){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
   constructor() { }
 
